2ad3b26435803445d33044f06aae236c16f8d7a0
BraveJean <1062182057@qq.com>
2018-11-09 16:27:14 +0100

    modify

--- app/src/main/java/com/cookingapp/Register.java
+++ app/src/main/java/com/cookingapp/Register.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
     private EditText etUserName;
     private EditText etPassword;
+    private EditText etCPassword;
+
     private Button bRegister;
     private Button bBack;
     private ProgressDialog progressDialog;
@@ -31,6 +34,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
+        etCPassword = findViewById(R.id.etCPassword);
         bRegister = findViewById(R.id.bRegister);
         bBack = findViewById(R.id.bBack);
         progressDialog = new ProgressDialog(this);
@@ -44,6 +48,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     private void registerUser(){
         String userName = etUserName.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
+        String Cpassword = etCPassword.getText().toString().trim();
 
         if (TextUtils.isEmpty( userName )){
             // userName is empty
@@ -57,23 +62,49 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
             //stopping the function execution further
             return;
         }
+
+        if (TextUtils.isEmpty( Cpassword )){
+            // password is empty
+            Toast.makeText(this,"Please Confirm Your Password", Toast.LENGTH_SHORT).show();
+            //stopping the function execution further
+            return;
+        }
+
         //if valid are ok,
         //show progressBar
         progressDialog.setMessage("Registering User...");
         progressDialog.show();
-        firebaseAuth.createUserWithEmailAndPassword( userName, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            //register succeed and logged in
-                            //start profile activity here
-                            //now disply
-                            Toast.makeText(Register.this,"Registered Successfully",Toast.LENGTH_LONG).show();
+        if (Cpassword.equals(password))  {
+            firebaseAuth.createUserWithEmailAndPassword(userName, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("FirebaseAuth", "createUserWithEmail:onComplete:" + task.isSuccessful());
+                            //                        if (!task.isSuccessful()) {
+                            //                            Log.d("FirebaseAuth", "onComplete: Failed=" + task.getException().getMessage()); //ADD THIS
+                            //
+                            //                            Toast.makeText(Register.this, "auth_failed",
+                            //                                    Toast.LENGTH_SHORT).show();
+                            //                        }
+
+                            //Log.d("FirebaseAuth", "onComplete" + task.getException().getMessage());
+                            if (task.isSuccessful()) {
+                                Log.d("FirebaseAuth", "onComplete: Successfully"); //ADD THIS
+                                //register succeed and logged in
+                                //start profile activity here
+                                //now disply
+                                Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                                progressDialog.dismiss();
+                            } else {
+                                Toast.makeText(Register.this, "Registered problem, Please try again", Toast.LENGTH_LONG).show();
+                            }
                         }
-                        else Toast.makeText(Register.this,"Registered problem, Please try again",Toast.LENGTH_LONG).show();
-                    }
-                });
+                    });
+        }
+        else{
+            progressDialog.dismiss();
+            Toast.makeText(Register.this, "Please enter your Password and confirm it again.", Toast.LENGTH_LONG).show();
+        }
     }
 
 
@@ -85,6 +116,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         }
         if(v == bBack){
             //will open login activity
+//            Intent intent = new Intent(Register.this, LoginActivety.class);
+//            startActivity(intent);
         }
     }
 }
