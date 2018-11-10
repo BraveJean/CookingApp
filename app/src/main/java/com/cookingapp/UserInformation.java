a7a3991b42e1bb8dcba47909cec931dd41f8214a
BraveJean <1062182057@qq.com>
2018-11-10 15:35:53 +0100

    add UserInform

---
+++ app/src/main/java/com/cookingapp/UserInformation.java
@@ -0,0 +1,15 @@
+package com.cookingapp;
+
+public class UserInformation {
+    public String userName;
+    public String password;
+
+    public  UserInformation(){
+
+    }
+
+    public UserInformation(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
